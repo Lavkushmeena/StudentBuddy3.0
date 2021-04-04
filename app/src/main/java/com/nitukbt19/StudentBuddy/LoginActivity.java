7e23d8ae7782858cfbf371673d7e6e6a9a59624f
Knox Arrow <krishanandangoswami@gmail.com>
2021-04-04 21:21:12 +0530

    Login ND Registration Completed with Logic Correction and Authentication done with pass word math and register button disable feature

--- app/src/main/java/com/nitukbt19/StudentBuddy/LoginActivity.java
+++ app/src/main/java/com/nitukbt19/StudentBuddy/LoginActivity.java
@@ -1,21 +1,13 @@
 package com.nitukbt19.StudentBuddy;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.nitukbt19.StudentBuddy.Models.Users;
 import com.nitukbt19.StudentBuddy.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -29,36 +21,26 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         // Progress Dialog
         progressDialog=new ProgressDialog(LoginActivity.this);
-        progressDialog.setTitle("Creating Account");
+        progressDialog.setTitle("Logging In");
         progressDialog.setMessage("Welcome Back");
-
-        binding.tvSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
-            }
+        //
+        binding.tvSignUp.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
+        binding.btnLogin.setOnClickListener(v -> {
+            progressDialog.show();
+            mauth.signInWithEmailAndPassword(binding.inputEmail.getText().toString(),binding.etPassword.getText().toString()).
+                    addOnCompleteListener(task -> {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    });
         });
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progressDialog.show();
-                mauth.signInWithEmailAndPassword(binding.inputEmail.getText().toString(),binding.etPassword.getText().toString()).
-                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressDialog.dismiss();
-                                if(task.isSuccessful()){
-                                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                                    startActivity(intent);
-                                }else{
-                                    Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-            }
-        });
-        if(mauth.getCurrentUser()!=null){
-            startActivity(new Intent(LoginActivity.this,MainActivity.class));
-        }
+//        if we want auto sign up()
+//        if(mauth.getCurrentUser()!=null){
+//            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+//        }
     }
 }
--- app/src/main/java/com/nitukbt19/StudentBuddy/RegisterActivity.java
+++ app/src/main/java/com/nitukbt19/StudentBuddy/RegisterActivity.java
@@ -1,19 +1,15 @@
 package com.nitukbt19.StudentBuddy;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.nitukbt19.StudentBuddy.Models.Users;
@@ -39,42 +35,59 @@ public class RegisterActivity extends AppCompatActivity {
         progressDialog=new ProgressDialog(RegisterActivity.this);
         progressDialog.setTitle("Creating Account");
         progressDialog.setMessage("Welcome "+binding.inputUsername.getText().toString());
-        binding.alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+        binding.alreadyHaveAccount.setOnClickListener(view -> {
+            Intent i=new Intent(RegisterActivity.this,LoginActivity.class);
+            startActivity(i);
+        });
+        //disabling button till all inputs are entered
+        binding.btnRegister.setEnabled(false);
+        binding.inputConfirmPassword1.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().length()>3&&binding.inputPasswordRegister2.getText().toString().length()>3){
+                    binding.btnRegister.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
-        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progressDialog.show();
-                //matching confirm password
-                if(binding.inputPasswordRegister2.getText().toString().equals(binding.inputConfirmPassword1.getText().toString())) {
-                    mAuth.createUserWithEmailAndPassword(binding.etEmailAddress.getText().toString(), binding.inputPasswordRegister2.getText().toString()).
-                            addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    progressDialog.dismiss();
-                                    if (task.isSuccessful()) {
-                                        Users user=new Users(binding.inputUsername.getText().toString(),binding.etEmailAddress.getText().toString(),binding.inputPasswordRegister2.getText().toString());
-                                        String id=task.getResult().getUser().getUid();
-                                        database.getReference().child("Users").child(id).setValue(user);
-                                        Toast.makeText(RegisterActivity.this, "Successfully Registered in Student Buddy", Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
 
-                                    }
-                                }
-                            });
-                }else{
-                    binding.inputPasswordRegister2.setText("");
-                    binding.inputConfirmPassword1.setText("");
-                    binding.inputPasswordRegister2.setHintTextColor(Color.RED);
-                    binding.inputConfirmPassword1.setHintTextColor(Color.RED);
-                }
 
+        // Register button Logic
+        binding.btnRegister.setOnClickListener(v -> {
+            progressDialog.show();
+            //matching confirm password
+            if(binding.inputPasswordRegister2.getText().toString().equals(binding.inputConfirmPassword1.getText().toString())) {
+                mAuth.createUserWithEmailAndPassword(binding.etEmailAddress.getText().toString(), binding.inputPasswordRegister2.getText().toString()).
+                        addOnCompleteListener(task -> {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+                                Users user=new Users(binding.inputUsername.getText().toString(),binding.etEmailAddress.getText().toString(),binding.inputPasswordRegister2.getText().toString());
+                                String id=task.getResult().getUser().getUid();
+                                database.getReference().child("Users").child(id).setValue(user);
+                                Toast.makeText(RegisterActivity.this, "Successfully Registered in Student Buddy", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+                // to add intent
+            }else{
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this,"Passwords do not match!!", Toast.LENGTH_LONG).show();
+                binding.inputPasswordRegister2.setText("");
+                binding.inputConfirmPassword1.setText("");
+                binding.inputPasswordRegister2.setHintTextColor(Color.RED);
+                binding.inputConfirmPassword1.setHintTextColor(Color.RED);
             }
+
         });
 
     }
